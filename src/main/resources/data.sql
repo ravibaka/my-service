ecfb6406a7d95b859da235406115b2d75b758424
ravibaka <bakaravi.c@gmail.com>
2018-12-11 11:31:00 +0100

    updated the emails

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,3 @@ values(10002,'Andre Aubert', 'andre.aubert@email.com', 'ACTIVE');
 
 insert into customer
 values(10003,'Frode Bjerkenes', 'Frode.Bjerkenes@email.com', 'REGISTERED');
-
-insert into customer
-values(10004,'Ravi Baka', 'ravi.baka@email.com', 'NOT REGISTERED');
